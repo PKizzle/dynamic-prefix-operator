94a847a33fc43fc8b87007858162c9552cabd514
Philipp Kolberg <philipp.kolberg@t-online.de>
2026-02-17 01:28:37 +0100

    chore: Update copyright info

--- api/v1alpha1/dynamicprefix_types.go
+++ api/v1alpha1/dynamicprefix_types.go
@@ -1,5 +1,6 @@
 /*
 Copyright 2026 jr42.
+Copyright 2026 PKizzle.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
